487172f5970acf1898711d2247eddb0a51ba7acd
Seif Jelidi <seifjelidi3@gmail.com>
2021-07-21 21:57:55 +0000

    fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 """
-   function that concat with annotations
+function that concat with annotations
 """
+
 
 def concat(str1: str, str2: str) -> str:
     return str(str1) + str(str2)
