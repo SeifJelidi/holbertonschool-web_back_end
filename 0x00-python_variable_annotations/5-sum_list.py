6088f49f183a000bcebab7babebbf6ceffd0fb48
Seif Jelidi <seifjelidi3@gmail.com>
2021-07-21 22:42:01 +0000

    fix pep8

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-'''
+"""
    type-annotated function sum_lis
-'''
+"""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """returns their sum as a float"""
     return sum(input_list)
