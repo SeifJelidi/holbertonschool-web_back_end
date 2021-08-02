8279cd22a0c9b2b06d231f558f638b7891337ea8
Seif Jelidi <seifjelidi3@gmail.com>
2021-08-02 16:56:51 +0000

    task 3

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 import re
 from typing import List
 import logging
+import os
+import mysql.connector
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -48,3 +50,15 @@ def get_logger() -> logging.Logger:
     sh.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(sh)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """returns a connector to the database"""
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', "root")
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', "")
+    host = os.getenv('PERSONAL_DATA_DB_HOST', "localhost")
+    name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    return mysql.connector.connect(user=username, password=password,
+                                  host=host,
+                                  database=name)
