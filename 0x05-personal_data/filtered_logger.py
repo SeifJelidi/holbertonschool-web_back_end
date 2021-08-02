4107dbb720223cd3ccea02c76871ca2443ea120e
Seif Jelidi <seifjelidi3@gmail.com>
2021-08-02 16:59:51 +0000

    fix task 3

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -58,7 +58,6 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', "")
     host = os.getenv('PERSONAL_DATA_DB_HOST', "localhost")
     name = os.getenv('PERSONAL_DATA_DB_NAME')
-
     return mysql.connector.connect(user=username, password=password,
-                                  host=host,
-                                  database=name)
+                                   host=host,
+                                   database=name)
