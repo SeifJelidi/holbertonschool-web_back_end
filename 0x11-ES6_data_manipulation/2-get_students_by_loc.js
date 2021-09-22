200547086a38f89c9227932dd874b11052fc0852
Your Name <you@example.com>
2021-09-22 22:24:15 +0100

    filter

--- 0x11-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(MyArr, city) {
-  let s1 = MyArr.filter((i) => i.location === city);
+  const s1 = MyArr.filter((i) => i.location === city);
   return s1;
 }
